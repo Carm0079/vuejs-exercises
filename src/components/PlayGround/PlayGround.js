b91ad7c738f01253bb295fb08bbadb4b5e2613e3
Alex Carmichael <carm0079@algonquinlive.com>
2019-01-26 23:21:28 -0500

    Finished Assignment 1

--- src/components/PlayGround/PlayGround.js
+++ src/components/PlayGround/PlayGround.js
@@ -2,34 +2,18 @@ export default {
   name: 'PlayGround',
   data() {
     return {
-      backgroundClassName: 'red-background',
-      counter: 0,
-      message: 'static message',
-      anchorTag: '<a href="https://www.google.ca" target="_blank">Heyuhhh</a>',
-      nameInput: 'No Name'
-    }
-  },
-
-  computed: {
-    reversedMessage() {
-      return this.message.toLowerCase().split('').reverse().join('')
-    }
-  },
-
-  watch: {
-    nameInput(newValue, oldValue) {
-      /* eslint-disable no-console */
-      console.log('old value: ', oldValue)
-      console.log('new value: ', newValue)
-    }
-  },
-
-  methods: {
-    handleClick(event) {
-      this.counter++
-      this.message = `you are clicked this ${this.counter} times!`
-      this.backgroundClassName = this.counter % 2 ? 'green-background' : 'red-background'
-      this.message = this.nameInput
+      // Form data. Holds all content written by the user.
+      form: {
+        firstname: '',
+        lastName: '',
+        email: '',
+        subject: '',
+        content: '',
+        // Checkbox starts off unchecked because we need the confirmation form the user.
+        agree: false
+      },
+      // The default visibility of the dialog box.
+      dialogVisible: false
     }
   }
 }
--- src/components/PlayGround/PlayGround.vue
+++ src/components/PlayGround/PlayGround.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="hello">
-    <!-- v-bind -->
-    <div v-bind:class="backgroundClassName">Some Class With Background</div>
-    <!-- v-on -->
-    <button v-on:click="handleClick"></button>
-    <!-- v-once -->
-    <div v-once>the first message will never change: {{message}}</div>
-    <div>the first message will never change: {{message}}</div>
-    <!-- v-html -->
-    <div v-html="anchorTag"></div>
-    <!-- v-model -->
-    <label for="firstName">First Name</label>
-    <input type="text" name="firstName" v-model="nameInput">
-    <!-- computed -->
-    <div>{{message}} => reversed: {{reversedMessage}}</div>
-    <!-- watch -->
-    <input type="text" name="firstName" v-model="nameInput">
+  <div>
+    <!-- Form for the user to send a claim request -->
+    <el-form ref="form" :model="form" label-width="120px">
+      <!-- Input field for the user to insert their first name -->
+      <el-form-item label="First Name">
+        <el-input placeholder="John" v-model="form.firstName" clearable></el-input>
+      </el-form-item>
+      <!-- Input field for the user to insert their last name -->
+      <el-form-item label="Last Name">
+        <el-input placeholder="Doe" v-model="form.lastName" clearable></el-input>
+      </el-form-item>
+      <!-- Input field for the user to insert their email -->
+      <el-form-item label="Email">
+        <el-input placeholder="email@example.com" v-model="form.email" clearable></el-input>
+      </el-form-item>
+      <!-- Input field for the user to type down the subject of the message -->
+      <el-form-item label="Subject">
+        <el-input placeholder="Example" v-model="form.subject" clearable></el-input>
+      </el-form-item>
+      <!-- Text area for the user to type the message -->
+      <el-form-item label="Content">
+        <el-input placeholder="Type here" type="textarea" v-model="form.content"></el-input>
+      </el-form-item>
+      <!-- Boolean checkbox for the user to agree to terms -->
+      <el-form-item>
+        <el-checkbox label="I agree to the terms & conditions" v-model="form.agree"></el-checkbox>
+      </el-form-item>
+      <!-- Submit button triggers the boolean of the dialogVisibility data return -->
+      <el-form-item>
+        <el-button type="primary" @click="dialogVisible = true">Submit Form</el-button>
+      </el-form-item>
+    </el-form>
+    <!-- Dialog box for confirmation of the submission -->
+    <el-dialog title="Confirmation" :visible.sync="dialogVisible" width="30%">
+      <span>Submit Request Form?</span>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="dialogVisible = false">Cancel</el-button>
+        <el-button type="primary" @click="dialogVisible = false">Confirm</el-button>
+      </span>
+    </el-dialog>
   </div>
 </template>
 
